05060a39d2789441be4a8edc6cee8be699180389
system-searies-avilible <111744571+system-searies-avilible@users.noreply.github.com>
2022-09-04 14:01:15 +0400

    Add files via upload

---
+++ file1.php
@@ -0,0 +1,20 @@
+<?php
+//variable function
+function name(){
+	echo"My name is moheez"."<br/>";
+	
+}
+
+
+
+$fun="name";
+$fun();
+
+//simple function
+function fname(){
+	echo"My father name is Abdul-Razzaq";
+	
+}
+
+fname()
+?>
